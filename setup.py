c63979e8415eded060042ea896d8c6ebb4008710
craigv <vear2ev2.co.uk>
2020-03-09 11:41:47 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='nautilus-v03',
+      version='1.0',
+      py_modules=['nautilus-v03'],
+      author='Craig Vear',
+      author_email='cvear@dmu.ac.uk',
+
+      )
