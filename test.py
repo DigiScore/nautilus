9bb793e81f2f4d54364b97a48a4944a481a51304
craigv <vear2ev2.co.uk>
2022-01-25 19:36:28 +0000

    sorting slideshow in PyQt5

--- test.py
+++ test.py
@@ -1,58 +1,123 @@
+import random
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow, QPushButton
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import QBasicTimer
 import glob
+from time import time
 
 class App(QWidget):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.image_files = glob.glob('data/images/*.png')
+
         self.setGeometry(100, 100, 900, 700)
-        # s = '<>'*300
-        self.label = QLabel(self)
-        # self.label.setGeometry(10, 30, 640, 480)
-        self.label.setGeometry(0, 0, 900, 700)
 
+        self.timer_dict = {"top_left": time(),
+                           "top_right": time(),
+                           "bottom_left": time(),
+                           "bottom_right": time()}
 
+        # create all labels
+        self.createLabels()
 
-        # self.button = QPushButton("Start Slide Show",self)
-        # self.button.setGeometry(10, 10, 140, 30)
-        # self.button.clicked.connect(self.timerEvent)
-        # self.timerEvzent()
-
-
+        # initiate slideshow
         self.timer = QBasicTimer()
         self.step = 0
         self.delay = 500  # milliseconds
 
+        self.slideshow()
 
-        # sf = "Slides are shown {} seconds apart"
-        # self.setWindowTitle(sf.format(self.delay/1000.0))
+    def createLabels(self):
+        screen_resolution = self.geometry()
+        height = screen_resolution.height()
+        width = screen_resolution.width()
 
-        self.timerEvent()
+        print(height, width)
+        # setting  the geometry of window
+        # self.setGeometry(0, 0, 400, 300)
+
+        # creating a label widget
+        self.top_left_label = QLabel(self)
+
+        # moving position
+        self.top_left_label.move(0, 0)
+
+        # setting up border
+        self.top_left_label.setStyleSheet("border: 1px solid black;")
+
+        # resizing the widget
+        self.top_left_label.resize(width / 2, height / 2)
+
+        # creating a label widget
+        self.top_right_label = QLabel(self)
+
+        # moving position
+        self.top_right_label.move(width / 2, 0)
+
+        # setting up border
+        self.top_right_label.setStyleSheet("border: 1px solid black;")
+
+        # resizing the widget
+        self.top_right_label.resize(width / 2, height / 2)
+
+        # creating a label widget
+        self.bottom_left_label = QLabel(self)
+
+        # moving position
+        self.bottom_left_label.move(0, height / 2)
+
+        # setting up border
+        self.bottom_left_label.setStyleSheet("border: 1px solid black;")
+
+        # resizing the widget
+        self.bottom_left_label.resize(width / 2, height / 2)
+
+        # creating a label widget
+        self.bottom_right_label = QLabel(self)
+
+        # moving position
+        self.bottom_right_label.move(width / 2, height / 2)
+
+        # setting up border
+        self.bottom_right_label.setStyleSheet("border: 1px solid black;")
+
+        # resizing the widget
+        self.bottom_right_label.resize(width / 2, height / 2)
 
 
-    def timerEvent(self, e=None):
-        if self.step >= len(self.image_files):
-            self.timer.stop()
-            self.button.setText('Slide Show Finished')
-            return
+    def slideshow(self):
+        # if self.step >= len(self.image_files):
+        #     self.timer.stop()
+        #     self.button.setText('Slide Show Finished')
+        #     return
         # self.timer.start(self.delay, self)
-        file = self.image_files[self.step]
-        image_duration = float(file[-7:-4]) * 1000
+        while True:
+            for key, value in self.timer_dict.items():
+                if value < time():
 
-        self.timer.start(image_duration * 3, self)
+                    file = self.image_files[random.randrange(len(self.image_files))]
+                    image_duration = (float(file[-7:-4]) * 1000) * 3
+                    self.timer_dict[key] = image_duration
 
-        self.pixmap = QPixmap(file)
-        scale = 2
+                # self.timer.start(image_duration * 3, self)
 
-        size = self.pixmap.size()
+                    self.pixmap = QPixmap(file)
+                # scale = 2
 
-        scaled_pixmap = self.pixmap.scaled(scale * size)
-        self.label.setPixmap(scaled_pixmap)
-        self.setWindowTitle("{} --> {}".format(str(self.step), file))
-        self.step += 1
+                # size = self.pixmap.size()
+
+                    # scaled_pixmap = self.pixmap.scaled(scale * size)
+                    if key == "top_left":
+                        self.top_left_label.setPixmap(self.pixmap)
+                    elif key == "top_rightt":
+                        self.top_right_label.setPixmap(self.pixmap)
+                    elif key == "bottom_left":
+                        self.bottom_left_label.setPixmap(self.pixmap)
+                    else:
+                        self.bottom_right_label.setPixmap(self.pixmap)
+                    # self.setWindowTitle("{} --> {}".format(str(self.step), file))
+                # self.step += 1
 
 
 
@@ -76,62 +141,3 @@ if __name__ == '__main__':
     image_viewer.show()
     sys.exit(app.exec_())
 
-
-
-# import sys
-# from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
-# from PyQt5.QtGui import QIcon, QPixmap
-#
-# import glob
-# from time import sleep
-#
-#
-# class App(QMainWindow):
-#     def __init__(self):
-#         super().__init__()
-#         print('1')
-#         self.images = glob.glob('data/images/*.png')
-#         self.title = 'PyQt5 image - pythonspot.com'
-#         self.left = 10
-#         self.top = 10
-#         self.width = 640
-#         self.height = 480
-#         self.initUI()
-#
-#     def initUI(self):
-#         print('2')
-#         # self.acceptDrops()
-#         self.setWindowTitle(self.title)
-#         self.setGeometry(self.left, self.top, self.width, self.height)
-#         # self.setStyleSheet('transparent')
-#         self.setAutoFillBackground(True)
-#
-#         # Create widget
-#         label = QLabel(self)
-#
-#         # pixmap = QPixmap('data/images/nautilus-1643040124.741466-d-0.5.png')
-#         # # pixmap.setDevicePixelRatio(2)
-#         #
-#         # label.setPixmap(pixmap)
-#         #
-#         # # self.resize(pixmap.width(), pixmap.height())
-#         #
-#         # print('3')
-#         # self.show()
-#
-#
-#         for img in self.images:
-#             print(img)
-#             pixmap = QPixmap(img)
-#             label.setPixmap(pixmap)
-#             self.resize(pixmap.width(), pixmap.height())
-#             # self.show()
-#             print('3')
-#             self.update()
-#             sleep(2)
-#
-#
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     ex = App()
-#     sys.exit(app.exec_())
